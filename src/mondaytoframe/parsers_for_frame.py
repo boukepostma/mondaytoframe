97b0b9dc0b11339ecf336c2995f0a347ab7423f1
Bouke Postma <33422940+boukepostma@users.noreply.github.com>
2025-05-01 09:22:34 +0000

    no need to validate_call anymore

--- src/mondaytoframe/parsers_for_frame.py
+++ src/mondaytoframe/parsers_for_frame.py
@@ -2,7 +2,6 @@ from datetime import datetime
 from typing import Any
 
 import pandas as pd
-from pydantic import validate_call
 from mondaytoframe.model import (
     CheckboxColumnValue,
     ColumnType,
@@ -17,70 +16,58 @@ from mondaytoframe.model import (
 )
 
 
-@validate_call()
 def parse_email_for_df(v: ColumnValue) -> Any:
     return v.text if v.text else None
 
 
-@validate_call()
 def parse_date_for_df(v: DateColumnValue) -> Any:
     if v.value is None or v.value.date is None:
         return pd.NaT
     return datetime.combine(v.value.date, v.value.time)
 
 
-@validate_call()
 def parse_text_for_df(v: ColumnValue):
     return v.text if v.text else None
 
 
-@validate_call()
 def parse_link_for_df(v: LinkColumnValue):
     if v.value is None:
         return None
     return v.value.url
 
 
-@validate_call()
 def parse_people_for_df(v: PeopleColumnValue):
     if v.value is None:
         return None
     return ",".join([v.id for v in v.value.personsAndTeams])
 
 
-@validate_call()
 def parse_status_for_df(v: ColumnValue):
     return v.text
 
 
-@validate_call()
 def parse_checkbox_for_df(v: CheckboxColumnValue) -> bool:
     return True if v.text else False
 
 
-@validate_call()
 def parse_tags_for_df(v: TagsColumnValue):
     return set([v.name for v in v.tags])
 
 
-@validate_call()
 def parse_long_text_for_df(v: ColumnValue):
     return v.text if v.text else None
 
 
-@validate_call()
 def parse_phone_for_df(v: PhoneColumnValue):
     if v.value is None:
         return None
     return f"{v.value.phone} {v.value.countryShortName}"
 
 
-@validate_call()
 def parse_dropdown_for_df(v: DropdownColumnValue):
     return set(v.label for v in v.values)
 
 
-@validate_call()
 def parse_numbers_for_df(v: NumberColumnValue):
     return v.text
 
